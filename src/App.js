11a2e3ae19df48bbbaea77e5691c24d2498330b0
Felix Icaza <fx.joliett17@gmail.com>
2021-08-12 22:23:32 +0000

    Clean project

--- src/App.js
+++ src/App.js
@@ -2,28 +2,10 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-filename-extension */
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Hello world</h1>;
 }
 
 export default App;
